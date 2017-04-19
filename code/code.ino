47e2f1cb9fa2690ef755465483e5a99a25f6a9bd
Teerasak Sathu <maxz_drum@icloud.com>
2017-04-19 16:26:56 +0700

    ให้เบน

--- code/code.ino
+++ code/code.ino
@@ -1,10 +1,15 @@
 #include <ESP8266WiFi.h>
 #include <FirebaseArduino.h>
-
+int state=LOW;
+int state1=LOW;
 int led1 = D1;
 int led2 = D2;
+int led = D3;
+int led3 = D4;
+int led4 = D5;
+int led5 = D6;
 
-
+int count = 0;
 
 //config firebas
 #define FIREBASE_HOST "testqueue-aaa05.firebaseio.com"
@@ -20,7 +25,10 @@ Serial.begin(115200);
 
   pinMode(led1,OUTPUT);
   pinMode(led2,OUTPUT);
-
+  pinMode(led3,OUTPUT);
+  pinMode(led4,OUTPUT);
+  pinMode(led,INPUT);
+  pinMode(led5,INPUT);
 
 
 
@@ -38,10 +46,14 @@ Serial.begin(115200);
   Serial.println("connecting :");
   Serial.print(WiFi.localIP());
 
-
+digitalWrite(led1,HIGH);
+digitalWrite(led2,HIGH);
+digitalWrite(led3,HIGH);
+digitalWrite(led4,HIGH);
 
 
   Firebase.begin(FIREBASE_HOST,FIREBASE_AUTH);
+  Firebase.set("num2",0);
 
 
 
@@ -50,8 +62,10 @@ Serial.begin(115200);
 }
 void loop(){
 
-  int num=Firebase.getInt("num");
+  counting();
 
+  int num=Firebase.getInt("num");
+  int num1=Firebase.getInt("num2");
 
   if(num==1){
     digitalWrite(led1,LOW);
@@ -62,8 +76,32 @@ void loop(){
    digitalWrite(led2,HIGH);
   }
 
+ if(num1 ==1){
+  digitalWrite(led3,LOW);
+ }else if(num1==5){
+  digitalWrite(led4,LOW);
+ }
+
   
-delay(2000);  
+delay(500);  
 }
 
+
+void counting(){
+ state=digitalRead(led);
+  state1=digitalRead(led5);
+ 
+  if(state == LOW  ){
+    count++;
+// Serial.println(count);
+    
+ 
+  }else if(state1 == LOW){
+    count--;
+  }
+  Firebase.set("num2",count);
+ 
+  
+
+}
 
