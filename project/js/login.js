7579369e7fd2a4195935359507320fb50258892b
I  Run a way at, ,No way are But is not have ex <benzzamakon2@gmail.com>
2017-04-21 15:00:44 +0700

    แก้ไขข้อมูลบางส่วน 21.4.2017

--- project/js/login.js
+++ project/js/login.js
@@ -1,21 +1,35 @@
 function BtnLogIn() {
-  var email = document.getElementById('txt_email').value;
-  var password = document.getElementById('txt_pass').value;
-firebase.auth().signInWithEmailAndPassword(email, password)
-  .then(function(user) {
-       // Success 
-       window.location.assign("page2.html");
-   })
-   .catch(function (err) {
-    // Handle Errors here.
-    var errorCode = err.code;
-    var errorMessage = err.message;
-    // var notErrorCode = user.code;
-    if (errorCode == 'auth/wrong-password') {
-      alert('Wrong password.');
-    } else {
-      alert(errorMessage);
-    } return;
-     }) 
-  
+    var email = document.getElementById('txt_email').value;
+    var password = document.getElementById('txt_pass').value;
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .catch(function(err) {
+            // Handle Errors here.
+            var errorCode = err.code;
+            var errorMessage = err.message;
+            // var notErrorCode = user.code;
+            if ((errorCode == 'auth/wrong-password')) {
+                if (errorCode == 'auth/wrong-password') {
+                    alert('Wrong password.');
+                } else {
+                    alert(errorMessage);
+                }
+                return;
+            } else {
+                window.location.assign("page2.html");
+            }
+            // firebase.auth().signInWithEmailAndPassword(email, password)
+            //     .then(function(user) {
+            //         // Success 
+            //         if (errorCode == 'auth/wrong-password') {
+            //             alert('Wrong password.');
+            //         } else {
+            //             window.location.assign("page2.html");
+            //         }
+
+
+            //     })
+
+        })
+
+
 };
