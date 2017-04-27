7cbe50a59fbc6c095f20c5515bb05db7a60d0774
Teerasak Sathu <maxz_drum@icloud.com>
2017-04-27 20:18:00 +0700

    …..

--- project v.0.2/js/login.js
+++ project v.0.2/js/login.js
@@ -13,8 +13,19 @@
 
 
         var checkStatusText = document.querySelectorAll(".col-md-3");
+<<<<<<< Updated upstream
         checkStatusText[0].onclick = function () { checkStatus1() };
         function checkStatus1() {
+=======
+<<<<<<< HEAD
+        checkStatusText[0].onclick = function() { checkStatus() };
+
+        function checkStatus() {
+=======
+        checkStatusText[0].onclick = function () { checkStatus1() };
+        function checkStatus1() {
+>>>>>>> origin/master
+>>>>>>> Stashed changes
             var x = document.querySelectorAll("#well-id");
             x[0].style.display = "none";
             var x = document.querySelectorAll("#well-id");
@@ -22,10 +33,27 @@
             // document.getElementById('room').textContent = "ห้อง S101";
             
 
+<<<<<<< Updated upstream
         };
         var checkStatusText = document.querySelectorAll(".col-md-3");
         checkStatusText[1].onclick = function () { checkStatus2() };
         function checkStatus2() {
+=======
+<<<<<<< HEAD
+
+        }
+
+        var checkStatusText = document.querySelectorAll(".col-md-3");
+        checkStatusText[1].onclick = function() { checkStatus() };
+
+        function checkStatus() {
+=======
+        };
+        var checkStatusText = document.querySelectorAll(".col-md-3");
+        checkStatusText[1].onclick = function () { checkStatus2() };
+        function checkStatus2() {
+>>>>>>> origin/master
+>>>>>>> Stashed changes
             var x = document.querySelectorAll("#well-id");
             x[0].style.display = "none";
             var x = document.querySelectorAll("#well-id");
@@ -35,8 +63,19 @@
 
         };
         var checkStatusText = document.querySelectorAll(".col-md-3");
+<<<<<<< Updated upstream
         checkStatusText[2].onclick = function () { checkStatus3() };
         function checkStatus3() {
+=======
+<<<<<<< HEAD
+        checkStatusText[2].onclick = function() { checkStatus() };
+
+        function checkStatus() {
+=======
+        checkStatusText[2].onclick = function () { checkStatus3() };
+        function checkStatus3() {
+>>>>>>> origin/master
+>>>>>>> Stashed changes
             var x = document.querySelectorAll("#well-id");
             x[0].style.display = "none";
             var x = document.querySelectorAll("#well-id");
@@ -46,8 +85,19 @@
 
         };
         var checkStatusText = document.querySelectorAll(".col-md-3");
+<<<<<<< Updated upstream
         checkStatusText[3].onclick = function () { checkStatus4() };
         function checkStatus4() {
+=======
+<<<<<<< HEAD
+        checkStatusText[3].onclick = function() { checkStatus() };
+
+        function checkStatus() {
+=======
+        checkStatusText[3].onclick = function () { checkStatus4() };
+        function checkStatus4() {
+>>>>>>> origin/master
+>>>>>>> Stashed changes
             var x = document.querySelectorAll("#well-id");
             x[0].style.display = "none";
             var x = document.querySelectorAll("#well-id");
@@ -91,18 +141,18 @@ function check(BtnLogIn) {
 
     firebase.auth().signInWithEmailAndPassword(email, password)
 
-        .catch(function (err) {
-            // Handle Errors here.
-            var errorCode = err.code;
-            var errorMessage = err.message;
+    .catch(function(err) {
+        // Handle Errors here.
+        var errorCode = err.code;
+        var errorMessage = err.message;
 
-            if (errorCode == 'auth/wrong-password') {
-                alert('Wrong password.');
-            } else {
-                alert(errorMessage);
-            }
-            return;
-        });
+        if (errorCode == 'auth/wrong-password') {
+            alert('Wrong password.');
+        } else {
+            alert(errorMessage);
+        }
+        return;
+    });
 
 
 
